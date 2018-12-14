41960eb78eda6e6e1c3bde4535d34b37913d0171
Yuki-Kurita <kurita.yuki.kq7@is.naist.jp>
2018-12-14 12:06:03 +0900

    home画面

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>INSPECT</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+</head>
+
+<body>
+  <!-- ナビゲーションバー -->
+  <header>
+    <nav>
+    <ul id="menu">
+      <li class="hamburger"><a href="#menu"><i class="fa fa-bars"></i></a></li>
+        <li class="logo"><a href="">INSPECT</a></li>
+        <li><a href="create.php">Create Account</a></li>
+        <li><a href="login.php">log in</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+
+  </main>
+</body>
